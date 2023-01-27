72a242df41e6616e2b49bf2ff76ee113bda878b3
chewam <gary@chewam.com>
2023-01-27 23:42:47 +0100

    chore: add csp

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
 const { version } = require("./package.json")
 
+const stylesHashes = [
+  "'sha256-nV/9MCvxRKsBwS6o3BVMzpGfnO4kb9yRBMAf2CEulIs='",
+  "'sha256-CSr5A7IS9dRw3Uo7TFEY+Os1KKjUjndCqZWzkW4uTaY='",
+].join(" ")
+
+const ContentSecurityPolicy = `
+  font-src 'self';
+  base-uri 'none';
+  object-src 'none';
+  default-src 'self';
+  script-src 'self' 'unsafe-inline';
+  style-src 'self' ${stylesHashes} 'unsafe-hashes';
+`
+
 const securityHeaders = [
   {
     value: "1; mode=block",
     key: "X-XSS-Protection",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
   },
 ]
 
