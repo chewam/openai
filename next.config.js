6550fa6b55b3392aaddb9696784ac09c1a070305
chewam <gary@chewam.com>
2023-01-27 19:22:33 +0100

    chore: add security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const { version } = require("./package.json")
 
+const securityHeaders = [
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
